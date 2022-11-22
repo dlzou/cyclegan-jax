355920ec0573bc08fb849bcf6314d3442ef6095b
Daniel Zou <zoudl2000@gmail.com>
2022-11-22 13:10:40 -0800

    Add python files

---
+++ gan.py
@@ -0,0 +1,6 @@
+""" Training and testing functions."""
+
+import numpy as np
+import jax
+import jax.numpy as jnp
+import flax.linen as nn
---
+++ networks.py
@@ -0,0 +1,18 @@
+""" Network components used by GAN.
+
+Refer to PyTorch implementation:
+https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/e2c7618a2f2bf4ee012f43f96d1f62fd3c3bec89/models/networks.py
+"""
+
+import numpy as np
+import jax
+import jax.numpy as jnp
+import flax.linen as nn
+
+
+class Generator(nn.Module):
+    pass
+
+
+class Discriminator(nn.Module):
+    pass
